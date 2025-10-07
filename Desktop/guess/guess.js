ee88da0004e802abdc4ecf97917e5afa8ddcc769
zzarial <89093593235ss@gmail.com>
2025-10-07 12:28:12 +0400

    пофиксил баги оформления и добавил уровни

--- Desktop/guess/guess.js
+++ Desktop/guess/guess.js
@@ -2,11 +2,18 @@ const prompt = require('prompt-sync')();
 
 let num = Math.floor(Math.random() * 100) + 1;
 console.log("Игра угадай число на js!");
+
+let lev = prompt("Выбери уровень:\n1 уровень (10 попыток)\n2 уровень (5 попыток): ");
+let att;
+
+if (lev == 1) {
+  att = 10;
+} else if (lev == 2) {
+  att = 5;
+}
 let vvod = prompt("Введи число: ");
-let att = 5;
-
-
 while (att > 0) {
+
   if (vvod > num) {
     vvod = prompt("МЕНЬШЕ: ");
     att = att - 1;
